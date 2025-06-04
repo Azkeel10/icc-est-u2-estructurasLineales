072325f1e80805e7c6fa5dd0cb0754d64b13bfd3
Azkeel10 <jismalt26@gmail.com>
2025-06-03 19:57:22 -0500

    1 parte

---
+++ src/App.java
@@ -0,0 +1,26 @@
+import Controllers.Stack;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("----------Programa funcionando----------");
+//-------------------------------------------------------------------------------
+        Stack stack = new Stack();
+
+        stack.push(10);
+        stack.push(-1);
+        stack.push(5);
+        stack.push(8);
+        
+        System.out.println("Tam= " + stack.size());
+
+        stack.printStack();
+        stack.pop();
+
+        System.out.println("\nTam= " + stack.size());
+
+        stack.printStack();
+
+
+    }
+}
